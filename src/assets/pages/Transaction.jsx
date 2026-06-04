f4e84f96c99c99eeaedf6f325a7ed09d53fe45b1
Sudan Basnet <83904391+SudanBasnet@users.noreply.github.com>
2026-06-04 16:56:38 +1000

    add: Transaction Form || Transaction Table

--- src/assets/pages/Transaction.jsx
+++ src/assets/pages/Transaction.jsx
@@ -1,13 +1,17 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { TransactionForm } from "../../components/layouts/TransactionForm";
+import { TransactionTable } from "../../components/layouts/TransactionTable";
 
 const Transaction = () => {
   return (
     <Container>
       <Row className="bg-dark p-5 rounded">
-        <Col md={6}>
-          <h1 className="text-light">Transaction History</h1>
+        <Col>
+          <TransactionForm />
+          <hr />
+          <TransactionTable />
         </Col>
       </Row>
     </Container>
